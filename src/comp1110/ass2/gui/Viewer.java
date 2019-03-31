a148e76841e91c092c75a42206b579a5ebb2000a
u6527600 <u6527600@anu.edu.au>
2019-03-31 23:25:28 +1100

    Viewer

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -36,8 +36,40 @@ public class Viewer extends Application {
      */
     void makePlacement(String placement) {
         // FIXME Task 4: implement the simple placement viewer
-
+        JFrame jFrame=new JFrame("Board");
+        //设置窗口的大小（7个格子，每个格子宽，高为100个像素）
+        jFrame.setSize(700,700);
+        //窗口的位置
+        jFrame.setLocation(300,300);
+        //格子的行数，列数
+        int m=7;
+        //格子的像素
+        int n=100;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                //在每个位置添加标签
+                JLabel jLabel=new JLabel();
+                //标签的大小为格子的大小
+                jLabel.setSize(n,n);
+                //标签的位置为索引乘以格子的高度/宽度
+                jLabel.setLocation(i*n,j*n);
+                //放置黑色方块
+                jLabel.setBackground(Color.WHITE);
+                //设置边框颜色
+                jLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                //设置不透明度为不透明
+                jLabel.setOpaque(true);
+                jFrame.add(jLabel);
+            }
+        }
+        //窗体设置可见
+        jFrame.setVisible(true);
+        //关闭退出
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+
+
 
     /**
      * Create a basic text field for input and a refresh button.
