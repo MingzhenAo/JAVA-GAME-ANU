3c9280cc4a5afd97c93a8ae9a2586fbbe11f30ee
u6605822 <u6605822@anu.edu.au>
2019-04-28 21:50:16 +1000

    originality update 3

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -6,13 +6,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import static comp1110.ass2.RailroadInk.*;
 
@@ -35,6 +35,7 @@ public class Viewer extends Application {
 
     /**
      * used to set the proper rotation
+     *
      * @param a
      * @param n
      * @return
@@ -47,13 +48,13 @@ public class Viewer extends Application {
             a.setScaleX(-1);
             n = n - 4;
         }
-        if (n == 1){
+        if (n == 1) {
             a.setRotate(90);
         }
-        if (n == 2){
+        if (n == 2) {
             a.setRotate(180);
         }
-        if (n == 3){
+        if (n == 3) {
             a.setRotate(270);
         }
         return a;
@@ -66,12 +67,11 @@ public class Viewer extends Application {
      */
     void makePlacement(String placement) {
         // FIXME Task 4: implement the simple placement viewer
-        if (placement.length() == 5){
+        if (placement.length() == 5) {
             placing(placement);
-        }
-        else {
+        } else {
             String[] placementArray = getPlacementStringArray(placement);
-            for (int i = 0; i < placementArray.length; i ++) {
+            for (int i = 0; i < placementArray.length; i++) {
                 placing(placementArray[i]);
             }
         }
@@ -79,160 +79,161 @@ public class Viewer extends Application {
 
     /**
      * Draw a placement or a sets of placements in the window, this is the method
+     *
      * @param placement
      */
     void placing(String placement) {
-        if (isTilePlacementWellFormed(placement)){
+        if (isTilePlacementWellFormed(placement)) {
             int n = 0;
-            switch (placement.substring(0,2)){
+            switch (placement.substring(0, 2)) {
                 case "A0":
-                    Image A0 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A0.png").toString());
+                    Image A0 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A0.png").toString());
                     ImageView a0 = new ImageView(A0);
                     a0.setFitHeight(80);
                     a0.setFitWidth(80);
-                    a0.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a0.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a0.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a0, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a0, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a0);
                     break;
                 case "A1":
-                    Image A1 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A1.png").toString());
+                    Image A1 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A1.png").toString());
                     ImageView a1 = new ImageView(A1);
                     a1.setFitHeight(80);
                     a1.setFitWidth(80);
-                    a1.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a1.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a1.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a1, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a1, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a1);
                     break;
                 case "A2":
-                    Image A2 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A2.png").toString());
+                    Image A2 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A2.png").toString());
                     ImageView a2 = new ImageView(A2);
                     a2.setFitHeight(80);
                     a2.setFitWidth(80);
-                    a2.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a2.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a2.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a2, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a2, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a2);
                     break;
                 case "A3":
-                    Image A3 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A3.png").toString());
+                    Image A3 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A3.png").toString());
                     ImageView a3 = new ImageView(A3);
                     a3.setFitHeight(80);
                     a3.setFitWidth(80);
-                    a3.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a3.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a3.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a3, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a3, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a3);
                     break;
                 case "A4":
-                    Image A4 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A4.png").toString());
+                    Image A4 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A4.png").toString());
                     ImageView a4 = new ImageView(A4);
                     a4.setFitHeight(80);
                     a4.setFitWidth(80);
-                    a4.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a4.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a4.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a4, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a4, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a4);
                     break;
                 case "A5":
-                    Image A5 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"A5.png").toString());
+                    Image A5 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "A5.png").toString());
                     ImageView a5 = new ImageView(A5);
                     a5.setFitHeight(80);
                     a5.setFitWidth(80);
-                    a5.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    a5.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     a5.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(a5, Integer.valueOf(placement.substring(4,5)));
+                    rotation(a5, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(a5);
                     break;
                 case "B0":
-                    Image B0 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"B0.png").toString());
+                    Image B0 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "B0.png").toString());
                     ImageView b0 = new ImageView(B0);
                     b0.setFitHeight(80);
                     b0.setFitWidth(80);
-                    b0.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    b0.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     b0.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(b0, Integer.valueOf(placement.substring(4,5)));
+                    rotation(b0, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(b0);
                     break;
                 case "B1":
-                    Image B1 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"B1.png").toString());
+                    Image B1 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "B1.png").toString());
                     ImageView b1 = new ImageView(B1);
                     b1.setFitHeight(80);
                     b1.setFitWidth(80);
-                    b1.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    b1.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     b1.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(b1, Integer.valueOf(placement.substring(4,5)));
+                    rotation(b1, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(b1);
                     break;
                 case "B2":
-                    Image B2 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"B2.png").toString());
+                    Image B2 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "B2.png").toString());
                     ImageView b2 = new ImageView(B2);
                     b2.setFitHeight(80);
                     b2.setFitWidth(80);
-                    b2.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    b2.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     b2.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(b2, Integer.valueOf(placement.substring(4,5)));
+                    rotation(b2, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(b2);
                     break;
                 case "S0":
-                    Image S0 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S0.png").toString());
+                    Image S0 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S0.png").toString());
                     ImageView s0 = new ImageView(S0);
                     s0.setFitHeight(80);
                     s0.setFitWidth(80);
-                    s0.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s0.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s0.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s0, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s0, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s0);
                     break;
                 case "S1":
-                    Image S1 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S1.png").toString());
+                    Image S1 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S1.png").toString());
                     ImageView s1 = new ImageView(S1);
                     s1.setFitHeight(80);
                     s1.setFitWidth(80);
-                    s1.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s1.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s1.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s1, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s1, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s1);
                     break;
                 case "S2":
-                    Image S2 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S2.png").toString());
+                    Image S2 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S2.png").toString());
                     ImageView s2 = new ImageView(S2);
                     s2.setFitHeight(80);
                     s2.setFitWidth(80);
-                    s2.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s2.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s2.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s2, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s2, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s2);
                     break;
                 case "S3":
-                    Image S3 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S3.png").toString());
+                    Image S3 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S3.png").toString());
                     ImageView s3 = new ImageView(S3);
                     s3.setFitHeight(80);
                     s3.setFitWidth(80);
-                    s3.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s3.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s3.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s3, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s3, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s3);
                     break;
                 case "S4":
-                    Image S4 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S4.png").toString());
+                    Image S4 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S4.png").toString());
                     ImageView s4 = new ImageView(S4);
                     s4.setFitHeight(80);
                     s4.setFitWidth(80);
-                    s4.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s4.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s4.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s4, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s4, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s4);
                     break;
                 case "S5":
-                    Image S5 = new Image(Viewer.class.getResource(Viewer.URI_BASE+"S5.png").toString());
+                    Image S5 = new Image(Viewer.class.getResource(Viewer.URI_BASE + "S5.png").toString());
                     ImageView s5 = new ImageView(S5);
                     s5.setFitHeight(80);
                     s5.setFitWidth(80);
-                    s5.setX(300 + 80 * Integer.valueOf(placement.substring(3,4)));
+                    s5.setX(300 + 80 * Integer.valueOf(placement.substring(3, 4)));
                     s5.setY(90 + 80 * (placement.charAt(2) - 'A'));
-                    rotation(s5, Integer.valueOf(placement.substring(4,5)));
+                    rotation(s5, Integer.valueOf(placement.substring(4, 5)));
                     root.getChildren().add(s5);
                     break;
                 default:
@@ -240,9 +241,6 @@ public class Viewer extends Application {
             }
         }
     }
-
-
-
 
 
     /**
@@ -269,12 +267,12 @@ public class Viewer extends Application {
     public void start(Stage primaryStage) throws Exception {
         //set board
         GridPane m = new GridPane();
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             ColumnConstraints column = new ColumnConstraints(80);
             m.getColumnConstraints().add(column);
         }
 
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             RowConstraints row = new RowConstraints(80);
             m.getRowConstraints().add(row);
         }
@@ -331,7 +329,7 @@ public class Viewer extends Application {
         root.getChildren().add(viewhigh6);
 
         //set railway exits
-        Image railexit = new Image(Viewer.class.getResource(Viewer.URI_BASE+"RailExit.png").toString());
+        Image railexit = new Image(Viewer.class.getResource(Viewer.URI_BASE + "RailExit.png").toString());
         ImageView viewrail1 = new ImageView(railexit);
         viewrail1.setX(530);
         viewrail1.setY(20);
@@ -378,7 +376,6 @@ public class Viewer extends Application {
         viewrail6.setFitWidth(100);
         viewrail6.setFitHeight(100);
         root.getChildren().add(viewrail6);
-
 
 
         primaryStage.setTitle("StepsGame Viewer");
@@ -557,6 +554,7 @@ public class Viewer extends Application {
             }
         }
     }
+
     //the method to drag the tiles
     private void dragTile(ImageView imageView) {
         imageView.setOnMouseDragged(mouseEvent -> {
@@ -585,14 +583,14 @@ public class Viewer extends Application {
 
 
     //check the tile is in position or not
-     private void inPosition(ImageView imageView) {
-         for (int i = 0; i < 8; i ++){
-             for (int j = 0; j < 8; j ++){
-                 if (imageView.getX() > 280 + i * 80 && imageView.getX() < 320 + i * 80 && imageView.getY() > 70 + j * 80 && imageView.getY() < 110 + j * 80) {
-                     imageView.setX(300 + i * 80);
-                     imageView.setY(90 + j * 80);
-                 }
-             }
-         }
+    private void inPosition(ImageView imageView) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (imageView.getX() > 280 + i * 80 && imageView.getX() < 320 + i * 80 && imageView.getY() > 70 + j * 80 && imageView.getY() < 110 + j * 80) {
+                    imageView.setX(300 + i * 80);
+                    imageView.setY(90 + j * 80);
+                }
+            }
+        }
     }
 }
