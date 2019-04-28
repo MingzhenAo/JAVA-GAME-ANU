2379794bcc42729a9ec8a88bf411552bd2f6ede5
u6527600 <u6527600@anu.edu.au>
2019-04-28 21:01:40 +1000

    task 5 task 8

--- src/comp1110/ass2/RailroadInk.java
+++ src/comp1110/ass2/RailroadInk.java
@@ -3,8 +3,10 @@ package comp1110.ass2;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static comp1110.ass2.ConnextedNeighbours.connectedneighbersornot;
 import static comp1110.ass2.DiceRoll.getMove;
 import static comp1110.ass2.TileRotate.getRotatedTile;
+import static comp1110.ass2.getbasicscore.*;
 import static java.lang.Character.getNumericValue;
 
 public class RailroadInk {
@@ -124,59 +126,7 @@ public class RailroadInk {
      */
     public static boolean areConnectedNeighbours(String tilePlacementStringA, String tilePlacementStringB) {
         // FIXME Task 5: determine whether neighbouring placements are connected
-
-
-        String a = tilePlacementStringA;
-        String b = tilePlacementStringB;
-
-
-        int[] tileA = getRotatedTile(a);
-        int[] tileB = getRotatedTile(b);
-
-
-        if (a.charAt(2) == b.charAt(2) && a.charAt(3) - b.charAt(3) == 1) //same row; a right; b left.
-        {
-
-            if (tileA[0] == tileB[2] && tileA[0] != 5)
-                return true;
-            /*
-            It means that only the left edge of tilePlacementStringA should be the same with the
-            right edge of tilePlacementStringB and the edges must exist(but we only need to determine one of them here).
-            */
-        }
-
-        if (a.charAt(2) == b.charAt(2) && b.charAt(3) - a.charAt(3) == 1) // same row; b right; a left.
-        {
-            if (tileA[2] == tileB[0] && tileA[2] != 5)
-                return true;
-              /*
-            It means that only the right edge of tilePlacementStringA should be the same with the
-            left edge of tilePlacementStringB and the edges must exist(but we only need to determine one of them here).
-            */
-        }
-
-        if (a.charAt(3) == b.charAt(3) && (int) a.charAt(2) - (int) b.charAt(2) == 1) // same column; b above; a below.
-        {
-            if (tileA[1] == tileB[3] && tileA[1] != 5)
-                return true;
-
-            /*
-            It means that only the top edge of tilePlacementStringA should be the same with the
-            bottom edge of tilePlacementStringB and the edges must exist(but we only need to determine one of them here).
-            */
-        }
-        if (a.charAt(3) == b.charAt(3) && (int) b.charAt(2) - (int) a.charAt(2) == 1) //same column; a above; b below
-        {
-            if (tileA[3] == tileB[1] && tileA[3] != 5)
-                return true;
-              /*
-            It means that only the bottom edge of tilePlacementStringA should be the same with the
-            top edge of tilePlacementStringB and the edges must exist(but we only need to determine one of them here).
-            */
-        }
-
-        //If all of these above are not satisfied, the placements are not connected neighbours.
-        return false;
+        return connectedneighbersornot(tilePlacementStringA,tilePlacementStringB);
     }
 
 
@@ -403,130 +353,6 @@ public class RailroadInk {
         return a;
     }
     /**
-     * @param s1
-     * @param s2
-     * @return
-     */
-
-    public static String handle(String s1, String s2) {
-        if (!s2.substring(0, 2).equals("B2")) {
-            return s2;
-        } else {
-            if (s1.charAt(2) == s2.charAt(2) && s1.charAt(3) - s2.charAt(3) == 1) {
-                if (getRotatedTile(s2)[2] == 0) {
-                    return "A4" + s2.substring(2, 5);
-                } else {
-                    return "A1" + s2.substring(2, 4) + "1";
-                }
-            }
-            if (s1.charAt(2) == s2.charAt(2) && s2.charAt(3) - s1.charAt(3) == 1)// same row,j right, i left
-            {
-
-                if (getRotatedTile(s2)[0] == 0) {
-                    return "A4" + s2.substring(2, 5);
-                } else {
-                    return "A1" + s2.substring(2, 4) + "1";
-                }
-            }
-            if (s1.charAt(3) == s2.charAt(3) && s1.charAt(2) - s2.charAt(2) == 1)//same column,i below, j top
-            {
-                if (getRotatedTile(s2)[3] == 0) {
-                    return "A4" + s2.substring(2, 5);
-                } else {
-                    return "A1" + s2.substring(2, 4) + "0";
-                }
-            }
-            if (s1.charAt(3) == s2.charAt(3) && s2.charAt(2) - s1.charAt(2) == 1)//same column,j below, i top
-            {
-
-                if (getRotatedTile(s2)[1] == 0) {
-                    return "A4" + s2.substring(2, 5);
-                } else {
-                    return "A1" + s2.substring(2, 4) + "0";
-                }
-            }
-        }
-        return s2;
-    }
-
-    /**
-     * @param hm2
-     * @param hm1
-     * @return
-     */
-    public static String add(String s1, String s2, HashMap<String, String> hm1, HashMap<String, String> hm2) {
-
-        String l = null;
-
-        if (areConnectedNeighbours(s1, s2)) {
-            String m = s2;
-            s2 = handle(s1, s2);
-            if (!hm1.containsKey(s2)) {
-                hm1.put(s2, s2.substring(2, 4));
-                hm2.put(s2, s2.substring(2, 4));
-            }
-            if (m.substring(0, 2).equals("B2")) {
-                l = s2;
-                s2 = m;
-            }
-        }
-        return l;
-    }
-
-
-    public static HashMap<String, String> line(ArrayList<String> a, HashMap<String, String> c) {
-        HashMap<String, String> e = new HashMap<>();
-        for (int i = 0; i < a.size(); i++) {
-            if (c.get(a.get(i)) != null) {
-                for (int k = 0; k < a.size(); k++) {
-                    if (areConnectedNeighbours(a.get(i), a.get(k))) {
-                        String m = a.get(k);
-                        a.set(k, handle(a.get(i), a.get(k)));
-                        if (!c.containsKey(a.get(k))) {
-                            a.add(a.get(k));
-                            c.put(a.get(k), a.get(k).substring(2, 4));
-                            e.put(a.get(k), a.get(k).substring(2, 4));
-                        }
-                        if (m.substring(0, 2).equals("B2")) {
-                            a.set(k, m);
-                        }
-
-                    }
-                }
-            }
-        }
-
-        return e;
-    }
-
-
-    /**
-     * @param boardString
-     * @return
-     */
-    public static ArrayList<HashMap<String, String>> exitsMapped(String boardString) {
-        String[] boardStringArray = getPlacementStringArray(boardString);
-        ArrayList<String> m = new ArrayList<>();
-        for (int i = 0; i < boardStringArray.length; i++) {
-            m.add(boardStringArray[i]);
-        }
-        HashMap<String, String> a = new HashMap<>();
-        ArrayList<HashMap<String, String>> b = new ArrayList<>();
-        for (int h = 0; h < m.size(); h++) {
-            if (a.get(m.get(h)) == null && !m.get(h).substring(0, 2).equals("B2")) {
-                a.put(m.get(h), m.get(h).substring(2, 4));
-                HashMap<String, String> n = line(m, a);
-                if (n.size() != 0) {
-                    n.put(m.get(h), m.get(h).substring(2, 4));
-                    b.add(n);
-                }
-            }
-        }
-        return b;
-    }
-
-
-    /**
      * Given the current state of a game board, output an integer representing the sum of all the following factors
      * that contribute to the player's final score.
      * <p>
@@ -540,132 +366,13 @@ public class RailroadInk {
     public static int getBasicScore(String boardString) {
         // FIXME Task 8: compute the basic score
         int score = 0;
-        String[] placementStringArray = getPlacementStringArray(boardString);
         //central tiles
-        for (int i = 0; i < placementStringArray.length; i++) {
-            if (placementStringArray[i].charAt(2) >= 'C' && placementStringArray[i].charAt(2) <= 'E') {
-                if (placementStringArray[i].charAt(3) >= '2' && placementStringArray[i].charAt(3) <= '4') {
-                    score++;
-                }
-            }
-        }
-
+        score+=centraltilesScore(boardString);
         //exits mapped
-
         //add the score according to the exits mapped
-        ArrayList<Integer> f = new ArrayList<>();
-        for (int i = 0; i < exitsMapped(boardString).size(); i++) {
-            HashMap<String, String> a = exitsMapped(boardString).get(i);
-            int count = 0;
-            for (String key : a.keySet()) {
-                if (a.get(key).equals("A1") && getRotatedTile(key)[1] != 5)
-                    count++;
-                if (a.get(key).equals("A3") && getRotatedTile(key)[1] != 5)
-                    count++;
-                if (a.get(key).equals("A5") && getRotatedTile(key)[1] != 5)
-                    count++;
-                if (a.get(key).equals("B0") && getRotatedTile(key)[0] != 5)
-                    count++;
-                if (a.get(key).equals("B6") && getRotatedTile(key)[2] != 5)
-                    count++;
-                if (a.get(key).equals("D0") && getRotatedTile(key)[0] != 5)
-                    count++;
-                if (a.get(key).equals("D6") && getRotatedTile(key)[2] != 5)
-                    count++;
-                if (a.get(key).equals("F0") && getRotatedTile(key)[0] != 5)
-                    count++;
-                if (a.get(key).equals("F6") && getRotatedTile(key)[2] != 5)
-                    count++;
-                if (a.get(key).equals("G1") && getRotatedTile(key)[3] != 5)
-                    count++;
-                if (a.get(key).equals("G3") && getRotatedTile(key)[3] != 5)
-                    count++;
-                if (a.get(key).equals("G5") && getRotatedTile(key)[3] != 5)
-                    count++;
-            }
-            f.add(count);
-        }
-
-        for (int j = 0; j < f.size(); j++) {
-            switch (f.get(j)) {
-                case 2:
-                    score += 4;
-                    break;
-                case 3:
-                    score += 8;
-                    break;
-                case 4:
-                    score += 12;
-                    break;
-                case 5:
-                    score += 16;
-                    break;
-                case 6:
-                    score += 20;
-                    break;
-                case 7:
-                    score += 24;
-                    break;
-                case 8:
-                    score += 28;
-                    break;
-                case 9:
-                    score += 32;
-                    break;
-                case 10:
-                    score += 36;
-                    break;
-                case 11:
-                    score += 40;
-                    break;
-                case 12:
-                    score += 45;
-                    break;
-            }
-        }
-
-
+        score+=exitsScore(boardString);
         //dead ends
-        TileRotate m = new TileRotate();
-        for (int i = 0; i < placementStringArray.length; i++) {
-            score -= 4; //assume every time it's got 4 dead ends
-            //get rid of the end that's blank or empty with the number 5
-            if (TileEnum.valueOf(placementStringArray[i].substring(0, 2)).left == 5)
-                score++;
-            if (TileEnum.valueOf(placementStringArray[i].substring(0, 2)).top == 5)
-                score++;
-            if (TileEnum.valueOf(placementStringArray[i].substring(0, 2)).right == 5)
-                score++;
-            if (TileEnum.valueOf(placementStringArray[i].substring(0, 2)).bottom == 5)
-                score++;
-            //if the tile was connected to the exit then plus 1
-            switch (placementStringArray[i].substring(2, 3)) {
-                case "A":
-                    if (getRotatedTile(placementStringArray[i])[1] != 5)
-                        score++;
-                    break;
-                case "G":
-                    if (getRotatedTile(placementStringArray[i])[3] != 5)
-                        score++;
-                    break;
-            }
-            switch (placementStringArray[i].substring(3, 4)) {
-                case "0":
-                    if (getRotatedTile(placementStringArray[i])[0] != 5)
-                        score++;
-                    break;
-                case "6":
-                    if (getRotatedTile(placementStringArray[i])[2] != 5)
-                        score++;
-                    break;
-            }
-            //any connected neighbors then plus 1
-            for (int j = 0; j < placementStringArray.length; j++) {
-                if (areConnectedNeighbours(placementStringArray[i], placementStringArray[j]))
-                    score++;
-            }
-        }
-
+        score+=getendScore(boardString);
         return score;
     }
 
