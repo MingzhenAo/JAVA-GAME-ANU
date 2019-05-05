5e8842a14df666ecea3b27be9bb795fd7d1b99b1
u6527600 <u6527600@anu.edu.au>
2019-05-05 23:19:58 +1000

    some test in task 8

--- tests/comp1110/ass2/getbasicscoremethodTest.java
+++ tests/comp1110/ass2/getbasicscoremethodTest.java
@@ -14,31 +14,35 @@ import static org.junit.Assert.assertEquals;
 public class getbasicscoremethodTest {
     @Rule
     public Timeout globalTimeout = Timeout.millis(2000);
+
     @Test
-    public void testcentraltilesScore()
-    {
-        String a="A2F45";
-        String b="A3C31A3C20A5D21S2D33B2C41B2D41A5E40A3E33B2E21";
-        assertEquals(centraltilesScore(a),0);
-        a+="B2E36";
-        assertEquals(centraltilesScore(a),1);
-        a+="A3D30A3D41";
-        assertEquals(centraltilesScore(a),3);
-        assertEquals(centraltilesScore(b),9);
+    public void testcentraltilesScore() {
+        String a = "A2F45";
+        String b = "A3C31A3C20A5D21S2D33B2C41B2D41A5E40A3E33B2E21";
+        assertEquals(centraltilesScore(a), 0);
+        a += "B2E36";
+        assertEquals(centraltilesScore(a), 1);
+        a += "A3D30A3D41";
+        assertEquals(centraltilesScore(a), 3);
+        assertEquals(centraltilesScore(b), 9);
+        a += b;
+        assertEquals(centraltilesScore(a), 9);
     }
+
     @Test
-    public void handletest()
-    {
-        String a="B2C31";
-        String b="A5C21";
-        assertEquals(handle(b,a),"A4C31");
-        assertEquals(handle(a,b),"A5C21");
+    public void handletest() {
+        String a = "B2C31";
+        String b = "A5C21";
+        String c = "A1B30";
+        String d ="";
+                assertEquals(handle(b, a), "A4C31");
+        assertEquals(handle(a, b), "A5C21");
     }
+
     @Test
-    public void linetest()
-    {
-        ArrayList<String> a=new ArrayList<>();
-        HashMap<String,String> b= new HashMap<>();
+    public void linetest() {
+        ArrayList<String> a = new ArrayList<>();
+        HashMap<String, String> b = new HashMap<>();
     }
 }
 
