af37af6eec0e5025c6f514585fddfbc4573c99d6
Yusen Wei <u6736425@anu.edu.au>
2019-05-06 20:07:27 +1000

    testRotate all tests finished

--- tests/comp1110/ass2/TileRotateTest.java
+++ tests/comp1110/ass2/TileRotateTest.java
@@ -38,6 +38,28 @@ public class TileRotateTest {
         }
     }
 
+    @Test
+    public void testRotateTimes(){
+        HashMap<String, Integer[]> rotateTimesValidTileMap = new HashMap<>(){{
+            put("S0B11", new Integer[] {1,0,0,0});
+            put("S1B42", new Integer[] {1,1,1,0});
+            put("S4B43", new Integer[] {0,1,1,0});
+            put("S5B44", new Integer[] {1,0,1,0});
+            put("A0B15", new Integer[] {5,5,1,1});
+            put("A1B46", new Integer[] {5,1,5,1});
+            put("A2B47", new Integer[] {1,5,1,1});
+            put("A3B41", new Integer[] {0,5,0,0});
+            put("A4B42", new Integer[] {5,0,5,0});
+            put("A5B43", new Integer[] {0,5,5,0});
+            put("B0B44", new Integer[] {5,0,5,1});
+            put("B1B45", new Integer[] {5,1,0,5});
+            put("B2B46", new Integer[] {1,0,1,0});
+        }};
+        for (var v : rotateTimesValidTileMap.keySet()){
+            testRotate(v, rotateTimesValidTileMap.get(v));
+        }
+    }
+
     private void testRotate(String placement, Integer[] validTile) {
         int[] resultTile = TileRotate.getRotatedTile(placement);
         Integer[] resultIntegerTile = new Integer[4];
@@ -49,7 +71,27 @@ public class TileRotateTest {
     }
 
     @Test
-    public void testRotateTimes(){
-        
+    public void testPerfectStation() {
+        String perfectStationS2 = "S2";
+        int[] s2Tile = {0,0,0,0};
+        String perfectStationS3 = "S3";
+        int[] s3Tile = {1,1,1,1};
+        perfectStationS2 += "XX";
+        perfectStationS3 += "XX";
+        for (int i = 0; i < 8; i ++) {
+            perfectStationS2 = perfectStationS2.substring(0, 4);
+            perfectStationS2 += i;
+            testStation(perfectStationS2, s2Tile);
+        }
+        for (int i = 0; i < 8; i ++) {
+            perfectStationS3 = perfectStationS3.substring(0, 4);
+            perfectStationS3 += i;
+            testStation(perfectStationS3, s3Tile);
+        }
+    }
+
+    private void testStation (String placement, int[] validTile) {
+        int[] resultTile = TileRotate.getRotatedTile(placement);
+        assertFalse("TileRotate.getRotatedTile(\"" + placement + "\") returned invalid tile: " + Arrays.toString(resultTile) + ", while the valid tile is: " + Arrays.toString(validTile), !Arrays.equals(validTile, resultTile));
     }
 }
