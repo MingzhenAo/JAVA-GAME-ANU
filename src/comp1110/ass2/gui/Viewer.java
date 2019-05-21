76d084ef1d79ad4fb7855827f563fae4c334aa29
Yusen Wei <u6736425@anu.edu.au>
2019-05-21 23:02:33 +1000

    better ending scene

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -591,7 +591,7 @@ public class Viewer extends Application {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("You have completed the game");
         alert.setHeaderText("");
-        alert.setContentText("You have completed the game, the basic score you got is: " +  + getBasicScore(boardString) + "/n" + "the total score you got is: " + getAdvancedScore(boardString));
+        alert.setContentText("You have completed the game\nBasic score: " + getBasicScore(boardString) + "\nTotal score: " + getAdvancedScore(boardString));
         alert.showAndWait();
     }
 
